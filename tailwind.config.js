4ec1a95632a43f97fdadbb69360796030f356232
rua-iri <rory.mcguigan@protonmail.com>
2024-02-22 11:59:03 +0300

    update: minor changes to colour scheme:

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,38 @@ module.exports = {
           "info": "#458588",
           "success": "#98971a",
           "warning": "#d65d0e",
-          "error": "#cc241d"
+          "error": "#cc241d",
+
+          "primary": "#d65d0e",
+          "secondary": "#af3a03",
+          "accent": "#427b58",
+          "neutral": "#3c3836",
+          "neutral-content": "#fbf1c7",
+          "base-100": "#ebdbb2",
+          "base-content": "#282828",
+          "--rounded-box": "0.25rem",
+          "--rounded-btn": ".125rem",
+          "--rounded-badge": ".125rem",
+          "--animation-btn": "0",
+          "--animation-input": "0",
+          "--btn-focus-scale": "1",
+
+          // "primary-focus": "#570df8",
+          // "primary-content": "#ffffff",
+          // "secondary-focus": "#689d6a",
+          // "secondary-content": "#f9f5d7",
+
+          // "accent-focus": "#389d6a",
+          // "accent-content": "#f9f5d7",
+          // "neutral": "#928374",
+          // "neutral-focus": "#a89984",
+
+          // "base-200": "#ebdbb2",
+          // "base-300": "#d5c4a1",
+          // "info": "#458588",
+          // "success": "#98971a",
+          // "warning": "#d65d0e",
+          // "error": "#cc241d"
         },
       },
       {
