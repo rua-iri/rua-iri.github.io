03ee41956e50bef9653230d27dba94778086e924
rua-iri <rory.mcguigan@protonmail.com>
2024-06-25 20:30:00 +0300

    update: minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,30 +15,6 @@ module.exports = {
     themes: [
       {
         lit: {
-          // ...require("daisyui/src/theming/themes")["[data-theme=corporate]"],
-          // "primary": "#793ef9",
-          // "primary-focus": "#570df8",
-          // "primary-content": "#ffffff",
-          // "secondary": "#af3a03",
-          // "secondary-focus": "#689d6a",
-          // "secondary-content": "#f9f5d7",
-
-          // "accent": "#427b58",
-          // "accent-focus": "#389d6a",
-          // "accent-content": "#f9f5d7",
-          // "neutral": "#928374",
-          // "neutral-focus": "#a89984",
-          // "neutral-content": "#f9f5d7",
-
-          // "base-100": "#fbf1c7",
-          // "base-200": "#ebdbb2",
-          // "base-300": "#d5c4a1",
-          // "base-content": "#282828",
-          // "info": "#458588",
-          // "success": "#98971a",
-          // "warning": "#d65d0e",
-          // "error": "#cc241d",
-
           "primary": "#d65d0e",
           "secondary": "#af3a03",
           "accent": "#427b58",
@@ -56,7 +32,6 @@ module.exports = {
       },
       {
         dim: {
-          // ...require("daisyui/src/theming/themes")["[data-theme=business]"],
           "color-scheme": "dark",
           "primary": "#1C4E80",
           "secondary": "#d65d0e",
